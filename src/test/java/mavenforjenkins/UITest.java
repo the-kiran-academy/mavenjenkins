bc974577ee83597ea747e3865989b1bd8c4c7c02
ram-chadar <javaprojectwork2715@gmail.com>
2023-01-03 16:35:40 +0530

    initial

---
+++ src/test/java/mavenforjenkins/UITest.java
@@ -0,0 +1,38 @@
+package mavenforjenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UITest {
+
+	String browserName = "Chrome";
+
+	@Test
+	public void startBrowser() {
+		System.out.println("Parameter value is " + browserName);
+		WebDriver driver = null;
+
+		if (browserName.contains("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions opt = new ChromeOptions();
+			opt.addArguments("--headless");
+			opt.addArguments("--no-sandbox");
+			opt.addArguments("--disable-dev-shm-usage");
+			driver = new ChromeDriver(opt);
+		} else if (browserName.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		driver.quit();
+	}
+
+}
